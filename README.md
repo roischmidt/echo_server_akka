3baa072c4d92de1874ede4fef55e07764fa7882a
rois <roi.schmidt77@gmail.com>
2017-10-27 15:43:36 +0300

    status added (request count and running time)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # echo_server_akka
 
 echo server that also returns different status code based on resource in path. e.g:
-http://localhost:1234/400 => returns 400
+http://localhost:1234/echo/400 => returns 400
--- src/main/scala/WebService.scala
+++ src/main/scala/WebService.scala
@@ -1,16 +1,30 @@
 
+import java.util.concurrent.TimeUnit
+import java.util.{Map => JMap}
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshalling.{Marshaller, ToEntityMarshaller}
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives.{entity, _}
 import akka.stream.ActorMaterializer
 import akka.util.ByteString
 import com.typesafe.config.ConfigFactory
 
+import scala.collection.JavaConversions._
 import scala.concurrent.ExecutionContextExecutor
 
 object WebServer {
     
+    val metricRegistry = new com.codahale.metrics.MetricRegistry()
+    
+    val requestMeter = metricRegistry.meter("echo.server.requestCount")
+    
+    val startTime = System.currentTimeMillis()
+    
+    implicit val mapMarshaller: ToEntityMarshaller[Map[String, Any]] = Marshaller.opaque { map =>
+        HttpEntity(ContentType(MediaTypes.`application/json`), map.toString)
+    }
     
     def main(args: Array[String]) {
         implicit val system: ActorSystem = ActorSystem("echo_server")
@@ -19,8 +33,9 @@ object WebServer {
         implicit val executionContext: ExecutionContextExecutor = system.dispatcher
         
         val route =
-            pathPrefix(IntNumber ?) { (code) =>
+            pathPrefix("echo" /  (IntNumber ?)) { (code) =>
                 val statusCode = code.getOrElse(200)
+                requestMeter.mark()
                 get {
                     complete(HttpResponse(status = statusCode))
                 } ~
@@ -39,7 +54,14 @@ object WebServer {
                                                 }
                                     }
                         }
-            }
+            } ~
+                pathPrefix("status") {
+                    get {
+                        val runningTime = System.currentTimeMillis() - startTime
+                        val requestCount = metricRegistry.getMeters().get("echo.server.requestCount").getCount
+                        complete(s"running for ${TimeUnit.MILLISECONDS.toSeconds(runningTime)} seconds. request count = $requestCount")
+                    }
+                }
         
         
         val port = ConfigFactory.load().getInt("server.port")
